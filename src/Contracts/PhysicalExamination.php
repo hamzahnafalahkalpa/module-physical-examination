c19de40f7058a6162fe888ec896f577691c47b01
Hamzah <hamzahnafalah@gmail.com>
2025-05-06 14:26:31 +0700

    update modules

--- src/Contracts/PhysicalExamination.php
+++ src/Contracts/PhysicalExamination.php
@@ -2,6 +2,6 @@
 
 namespace Hanafalah\ModulePhysicalExamination\Contracts;
 
-use Hanafalah\LaravelSupport\Contracts\DataManagement;
+use Hanafalah\LaravelSupport\Contracts\Supports\DataManagement;
 
 interface PhysicalExamination extends DataManagement {}
