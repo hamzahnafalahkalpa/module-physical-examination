55cc0ac8addbeb08432413876ed72155308d9e24
hamzah <hamzahnafalah@gmail.com>
2025-10-23 16:38:03 +0700

    pemeriksaan fisik

--- src/Models/PhysicalExamination.php
+++ src/Models/PhysicalExamination.php
@@ -3,28 +3,42 @@
 namespace Hanafalah\ModulePhysicalExamination\Models;
 
 use Hanafalah\ModuleExamination\Models\Examination\Assessment\Assessment;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class PhysicalExamination extends Assessment
 {
-    protected $table        = 'assessments';
-    public $response_model  = 'array';
+    protected $table = 'assessments';
     public $specific = [
-        'anatomy_id',
-        'anatomy_name',
-        'coordinate',
-        'condition'
+        'female_body_form','female_muscle_form','male_body_form','male_muscle_form'
     ];
 
-    public function getExamResults(?Model $model = null): array
-    {
-        $model ??= $this;
-        $anatomy = $this->AnatomyModel()->findOrFail($model->anatomy_id);
-        return [
-            'anatomy_id'   => $model->anatomy_id,
-            'anatomy_name' => $anatomy->name,
-            'coordinate'   => $model->coordinate,
-            'condition'    => $model->condition
-        ];
+
+    public function getExams(mixed $default = null,? array $vars = null): array{
+        $result = [];
+        $specifics = $this->specific;
+        foreach ($specifics as $var) {
+            if (isset(request()->sex) && in_array(request()->sex,['female','male'])){
+                if (!Str::contains($var,request()->sex)) continue;
+                $default = $this->getDefaultForm($var);
+                $var = Str::after($var,request()->sex.'_');
+            }else{
+                $default = $this->getDefaultForm($var);
+            }
+            $result[$var] = $default;
+        }
+        return ['exam' => $result];
+    }
+
+    private function getDefaultForm(string $specific){
+        switch ($specific) {
+            default:
+                return [
+                    "asset_url" => physical_asset_url('/assets/'.$specific.'.webp'),
+                    'label' => 'Head to Toe',
+                    'morph' => 'HeadToToe',
+                    'data' => []
+                ];
+            break;
+        }
     }
 }
