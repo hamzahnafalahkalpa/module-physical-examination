3cfd0cba485b981b9d1ae955365420d349f9e03f
hamzah <hamzahnafalah@gmail.com>
2025-10-27 11:23:55 +0700

    update assessment

--- src/Models/PhysicalExamination.php
+++ src/Models/PhysicalExamination.php
@@ -3,24 +3,38 @@
 namespace Hanafalah\ModulePhysicalExamination\Models;
 
 use Hanafalah\ModuleExamination\Models\Examination\Assessment\Assessment;
+use Hanafalah\ModulePhysicalExamination\Resources\PhysicalExamination\{
+    ViewPhysicalExamination, ShowPhysicalExamination
+};
 use Illuminate\Support\Str;
 
 class PhysicalExamination extends Assessment
 {
     protected $table = 'assessments';
     public $specific = [
-        'female_body_form','female_muscle_form','male_body_form','male_muscle_form'
+        'female_body_form','female_muscle_form','male_body_form','male_muscle_form',
+        'child_form','baby_form'
     ];
-
 
     public function getExams(mixed $default = null,? array $vars = null): array{
         $result = [];
         $specifics = $this->specific;
         foreach ($specifics as $var) {
-            if (isset(request()->sex) && in_array(request()->sex,['female','male'])){
-                if (!Str::contains($var,request()->sex)) continue;
-                $default = $this->getDefaultForm($var);
-                $var = Str::after($var,request()->sex.'_');
+            $is_request_sex = isset(request()->sex);
+            $is_request_patient_id = isset(request()->patient_id);
+            if ($is_request_patient_id || $is_request_sex){
+                if ($is_request_patient_id){
+                    $patient = $this->PatientModel()->findOrFail(request()->patient_id);
+                    $is_request_sex = true;
+                    request()->merge([
+                        'sex' => $patient->prop_people['sex']
+                    ]);
+                }
+                if ($is_request_sex){
+                    if (!Str::contains($var,request()->sex)) continue;
+                    $default = $this->getDefaultForm($var);
+                    $var = Str::after($var,request()->sex.'_');
+                }
             }else{
                 $default = $this->getDefaultForm($var);
             }
@@ -31,7 +45,10 @@ class PhysicalExamination extends Assessment
 
     private function getDefaultForm(string $specific){
         switch ($specific) {
-            default:
+            case 'female_body_form':
+            case 'male_body_form':
+            case 'child_form':
+            case 'baby_form':
                 return [
                     "asset_url" => physical_asset_url('/assets/'.$specific.'.webp'),
                     'label' => 'Head to Toe',
@@ -39,6 +56,23 @@ class PhysicalExamination extends Assessment
                     'data' => []
                 ];
             break;
+            case 'female_muscle_form':
+            case 'male_muscle_form':
+                return [
+                    "asset_url" => physical_asset_url('/assets/'.$specific.'.webp'),
+                    'label' => 'Head to Toe (Muskular)',
+                    'morph' => 'HeadToToe',
+                    'data' => []
+                ];
+            break;
         }
+    }
+
+    public function getViewResource(){
+        return ViewPhysicalExamination::class;
+    }
+
+    public function getShowResource(){
+        return ShowPhysicalExamination::class;
     }
 }
