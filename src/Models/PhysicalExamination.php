bd9429b9d905483d8f07467cc6f3e4477ae068cd
hamzah <hamzahnafalah@gmail.com>
2025-11-30 12:33:54 +0700

    update seeder dan hq

--- src/Models/PhysicalExamination.php
+++ src/Models/PhysicalExamination.php
@@ -13,7 +13,7 @@ class PhysicalExamination extends Assessment
     protected $table = 'assessments';
     public $specific = [
         'female_body_form','female_muscle_form','male_body_form','male_muscle_form',
-        'child_form','baby_form','odontogram'
+        'child_form','baby_form'
     ];
 
     public function getExams(mixed $default = null,? array $vars = null): array{
@@ -25,9 +25,6 @@ class PhysicalExamination extends Assessment
             if ($is_request_patient_id || $is_request_sex){
                 if ($is_request_patient_id){
                     $patient = $this->PatientModel()->findOrFail(request()->patient_id);
-                    request()->merge([
-                        'sex' => $patient->prop_people['sex']
-                    ]);
                     $patient_segment = $this->patientSegment($patient->prop_people['dob']);
                     $is_bayi_balita = false;
                     $is_anak_anak = false;
@@ -38,23 +35,22 @@ class PhysicalExamination extends Assessment
                     if ($patient_segment == 'anak-anak'){
                         $is_anak_anak = true;
                     }
-                    if (!$is_bayi_balita && !$is_anak_anak && $var !== 'odontogram'){
-                        $is_request_sex = true;                        
+                    if (!$is_bayi_balita && !$is_anak_anak){
+                        $is_request_sex = true;
+                        // request()->merge([
+                        //     'sex' => $patient->prop_people['sex']
+                        // ]);
                     }
                 }
-                if ($var == 'odontogram'){
+                if ($is_request_sex){
+                    if (!Str::contains($var,request()->sex)) continue;
                     $default = $this->getDefaultForm($var);
-                }else{
-                    if ($is_request_sex){
-                        if (!Str::contains($var,request()->sex)) continue;
-                        $default = $this->getDefaultForm($var);
-                        $var = Str::after($var,request()->sex.'_');
-                    }
-                    if ($is_bayi_balita || $is_anak_anak){
-                        if ($is_bayi_balita && !Str::contains($var,'baby')) continue;
-                        if ($is_anak_anak && !Str::contains($var,'child')) continue;
-                        $default = $this->getDefaultForm($var);
-                    }                
+                    $var = Str::after($var,request()->sex.'_');
+                }
+                if ($is_bayi_balita || $is_anak_anak){
+                    if ($is_bayi_balita && !Str::contains($var,'baby')) continue;
+                    if ($is_anak_anak && !Str::contains($var,'child')) continue;
+                    $default = $this->getDefaultForm($var);
                 }
             }else{
                 $default = $this->getDefaultForm($var);
@@ -83,14 +79,6 @@ class PhysicalExamination extends Assessment
                     "asset_url" => physical_asset_url('/assets/'.$specific.'.webp'),
                     'label' => 'Head to Toe (Muskular)',
                     'morph' => 'HeadToToe',
-                    'data' => []
-                ];
-            break;
-            case 'odontogram':
-                return [
-                    "asset_url" => physical_asset_url('/assets/'.'odontogram.svg'),
-                    'label' => 'Odontogram',
-                    'morph' => 'Odontogram',
                     'data' => []
                 ];
             break;
